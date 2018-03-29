b9be2b100c59eeb8482fe65cc38297b90767b50f
TheDancerCodes <rojtaracha@gmail.com>
2018-03-29 23:42:58 +0300

    [FIX] Fix violations exposed by Java Static Code Analysis Tools.

--- app/src/androidTest/java/com/andela/mrm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/andela/mrm/ExampleInstrumentedTest.java
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,6 +16,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    /**
+     * Test Method that checks for the use of the Application Context.
+     *
+     * @throws Exception - Throws an Exception if there's any.
+     */
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
--- app/src/main/java/com/andela/mrm/MainActivity.java
+++ app/src/main/java/com/andela/mrm/MainActivity.java
@@ -3,8 +3,16 @@ package com.andela.mrm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * MainActivity Class.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * onCreate Method.
+     *
+     * @param savedInstanceState - Used to SAve/Restore Application State.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
