4da501a17c068f51a468d69625637502b9b57c86
Okonji Emmanuel <ebukeokonji@gmail.com>
2018-04-09 13:34:55 +0100

    feature(country-selection): Add country selection activity (#4)
    - create xml file for the fragment and the parent activity
    - inflate the layout of the fragment

---
+++ app/src/main/java/com/andela/mrm/CountryFragment.java
@@ -0,0 +1,20 @@
+package com.andela.mrm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by andeladeveloper on 06/04/2018.
+ */
+
+public class CountryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.country_fragment, container, false);
+    }
+}
