c98275b698f37b9384c47a964db656a5cbb0b03a
Okonji Emmanuel <ebukeokonji@gmail.com>
2018-04-16 12:21:24 +0100

    bug(fix-country-activity):cater for other screen sizes (#8)
    - provide constraint for other screen sizes

--- app/src/main/java/com/andela/mrm/room_booking/CountryFragment.java
+++ app/src/main/java/com/andela/mrm/room_booking/CountryFragment.java
@@ -17,6 +17,6 @@ public class CountryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.country_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_country, container, false);
     }
 }
