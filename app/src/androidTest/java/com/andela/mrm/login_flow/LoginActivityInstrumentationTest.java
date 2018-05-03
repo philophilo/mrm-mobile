4247d4c5168f5207d39a0dee014bf01c5cf95ec3
Chinwoke (Chike) Hyginus <hyginuschinwoke@gmail.com>
2018-05-03 17:52:58 +0100

    (#157085610) Write user interface tests for login_activity class
    - install required test(espresso) dependencies
    - add parent layout id to constraint_layout of activity_login.xml file
    - extract text strings to string resource file
    - modify image_slider_adapter class based on extracted strings
    - write comprehensive user interface test cases for login_activity class
    - delete unnecessary dummy instrumentation test
    - modify circle ci config file for proper tests execution practices
    
    [Finishes #157085610]

---
+++ app/src/androidTest/java/com/andela/mrm/login_flow/LoginActivityInstrumentationTest.java
@@ -0,0 +1,228 @@
+package com.andela.mrm.login_flow;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.view.ViewPager;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.andela.mrm.R;
+import com.andela.mrm.room_booking.country.CountryActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.hasChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * LoginActivity Instrumentation(UI) Test.
+ */
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule =
+            new ActivityTestRule<>(LoginActivity.class);
+
+    /**
+     * Test case that describes/tests for the visibility(properly loaded) of the LoginActivity.
+     * And its fragment components
+     *
+     * @throws Exception - throws an exception error when test fails
+     */
+    @Test
+    public void test1_ActivityFragmentsAreLoadedCorrectly() throws Exception {
+        onView(withId(R.id.frame_first))
+                .check(matches(allOf(
+                        isCompletelyDisplayed(),
+                        (not(isClickable())),
+                        hasChildCount(1))));
+
+        onView(withId(R.id.frame_second))
+                .check(matches(allOf(
+                        isCompletelyDisplayed(),
+                        hasChildCount(3))));
+    }
+
+    /**
+     * Test case that describes/tests for the visibility and functionality of the ViewPager adapter.
+     *
+     * @throws Exception - throws an exception error when test fails
+     */
+    @Test
+    public void test2_ViewPagerDisplaysAndFunctionsCorrectly() throws Exception {
+        onView(withId(R.id.view_pager_layout))
+                .check(matches(isCompletelyDisplayed()));
+
+        onView(withId(R.id.dot_layout))
+                .check(matches(isCompletelyDisplayed()));
+
+        final int numberOfPages = 3;
+
+        final int[] headings = {
+                R.string.onBoarding_header_one,
+                R.string.onBoarding_header_two,
+                R.string.onBoarding_header_three
+        };
+
+        final int[] descriptions = {
+                R.string.onBoarding_description_one,
+                R.string.onBoarding_description_two,
+                R.string.onBoarding_description_three
+        };
+        for (int i = 0; i < numberOfPages; i++) {
+            // Checks that the ViewPager is currently in its (i)th page/pane
+            onView(withId(R.id.view_pager_layout)).check(matches(inPage(i)));
+
+            if (i <= numberOfPages - 2) { // perform swipe if not on last page
+                onView(allOf(withId(R.id.image_slide), withContentDescription("slider image"),
+                        childAtPosition(withParent(withId(R.id.view_pager_layout)), 1),
+                        isCompletelyDisplayed()))
+                        .check(matches(isCompletelyDisplayed()));
+
+                onView(allOf(withId(R.id.text_header), childAtPosition(
+                        withParent(withId(R.id.view_pager_layout)),
+                        2), isCompletelyDisplayed()))
+                        .check(matches(withText(headings[i])));
+
+                onView(allOf(withId(R.id.text_description), childAtPosition(
+                        withParent(withId(R.id.view_pager_layout)),
+                        3), isDisplayed(), withText(descriptions[i])))
+                        .check(matches(withText(descriptions[i])));
+
+                onView(allOf(withId(R.id.view_pager_layout), childAtPosition(allOf(
+                        withId(R.id.frame_second), childAtPosition(
+                                withId(R.id.layout_login), 1)),
+                        0)))
+                        .perform(swipeLeft());
+            } else {
+
+                onView(
+                        allOf(withId(R.id.image_slide), withContentDescription("slider image"),
+                                childAtPosition(
+                                        withParent(withId(R.id.view_pager_layout)),
+                                        1),
+                                isCompletelyDisplayed()))
+                        .check(matches(isCompletelyDisplayed()));
+
+                onView(allOf(withId(R.id.text_header), childAtPosition(
+                        withParent(withId(R.id.view_pager_layout)),
+                        2), isCompletelyDisplayed()))
+                        .check(matches(withText(headings[i])));
+
+                onView(allOf(hasSibling(withId(R.id.text_header)),
+                        hasSibling(withId(R.id.image_slide)),
+                        withId(R.id.text_description),
+                        withText(descriptions[i])
+                )).check(matches(withText(descriptions[i])));
+            }
+        }
+    }
+
+    /**
+     * Test case that describes/tests for the visibility and functionality of the login button.
+     *
+     * @throws Exception - throws an exception error when test fails
+     */
+    @Test
+    public void test3_LoginButtonDisplaysAndFunctionsCorrectly() throws Exception {
+        Intents.init();
+
+        ViewInteraction loginButton = onView(
+                allOf(withId(R.id.button_login),
+                        withContentDescription("google login button"),
+                        childAtPosition(childAtPosition(
+                                withId(R.id.frame_first), 0), 0)));
+
+        loginButton
+                .check(matches(isDisplayed())) //checks that the button is displayed
+                .perform(click()); // checks that the button is clickable
+
+        mActivityTestRule.launchActivity(new Intent());
+
+        intended(hasComponent(CountryActivity.class.getName())); // checks that the CountryActivity
+                                                                 // is launched
+
+        Intents.release();
+    }
+
+    /**
+     * Returns a matcher that matches {@link ViewGroup}s based on targeted view hierarchy(position).
+     *
+     * @param parentMatcher - the current parent view target
+     * @param position      - position of the view in hierarchy
+     * @return boolean
+     */
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    /**
+     * Returns a matcher that matches {@link ViewPager}s based on currentItem property value.
+     *
+     * @param page {@link Matcher} of ViewPager's currentItem
+     * @return boolean
+     */
+    @NonNull
+    public static Matcher<View> inPage(final int page) {
+
+        return new BoundedMatcher<View, ViewPager>(ViewPager.class) {
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("in page: " + page);
+            }
+
+            @Override
+            public boolean matchesSafely(final ViewPager viewPager) {
+                return viewPager.getCurrentItem() == page;
+            }
+        };
+    }
+}
--- app/src/main/java/com/andela/mrm/adapter/ImageSliderAdapter.java
+++ app/src/main/java/com/andela/mrm/adapter/ImageSliderAdapter.java
@@ -1,6 +1,7 @@
 package com.andela.mrm.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,6 @@ import com.andela.mrm.R;
 /**
  * ImageSliderAdapter class.
  * Adds the three onBoarding views
- *
  */
 
 public class ImageSliderAdapter extends PagerAdapter {
@@ -22,14 +22,16 @@ public class ImageSliderAdapter extends PagerAdapter {
             R.drawable.ic_meeting, R.drawable.ic_room_status, R.drawable.ic_rating
     };
 
-    private final String[] slideHeaders = {
-            "EASIER MEETINGS", "MEETING ROOM STATUS", "GIVE FEEDBACK"
+    private final int[] slideHeaders = {
+            R.string.onBoarding_header_one,
+            R.string.onBoarding_header_two,
+            R.string.onBoarding_header_three
     };
 
-    private final String[] slideDesc = {
-            "Meetings are now even more fun with the MRM App",
-            "View meeting rooms status across the whole office space",
-            "Give feedback on meeting rooms right from the meeting room without stepping out"
+    private final int[] slideDescriptions = {
+            R.string.onBoarding_description_one,
+            R.string.onBoarding_description_two,
+            R.string.onBoarding_description_three
     };
 
     @Override
@@ -38,13 +40,14 @@ public class ImageSliderAdapter extends PagerAdapter {
     }
 
     @Override
-    public boolean isViewFromObject(View view, Object object) {
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
 
         return view.equals(object);
     }
 
+    @NonNull
     @Override
-    public Object instantiateItem(ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
         container.getContext();
         LayoutInflater layoutInflater = (LayoutInflater) container.getContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -59,7 +62,7 @@ public class ImageSliderAdapter extends PagerAdapter {
 
         slideImage.setImageResource(slideImages[position]);
         slideHeading.setText(slideHeaders[position]);
-        slideDescription.setText(slideDesc[position]);
+        slideDescription.setText(slideDescriptions[position]);
 
         container.addView(view);
 
@@ -67,7 +70,7 @@ public class ImageSliderAdapter extends PagerAdapter {
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView((View) object);
     }
 }
