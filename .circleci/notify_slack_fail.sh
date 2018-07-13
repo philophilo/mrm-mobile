937a9189dfe3b12fdc8ad145d55804e5a7d4f127
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-07-13 20:20:33 +0300

    #158839934 fix circleci notifications on failure (#71)
    * [Chore #158839934] test deploy
    
    * [Chore #158839934] test failure
    
    * [Chore #158839934] create environment variable to determine slack notification on failure
    
    * [Chore #158839934]  revert to testing all workflows
    
    * [Chore #158839934] remove repeated steps
    
    * [Chore #158839934] revert to copying java file

--- .circleci/notify_slack_fail.sh
+++ .circleci/notify_slack_fail.sh
@@ -15,8 +15,9 @@ declare_env_variables() {
 
 
   # Retrieving the urls for the CircleCI artifacts
-
-  CIRCLE_ARTIFACTS_URL="$(curl https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts?circle-token=${CIRCLE_TOKEN} | grep -o 'https://[^"]*')"
+  if [ "$INSTRUMENT_TEST_ARTIFACT" != exempted ]; then
+    CIRCLE_ARTIFACTS_URL="$(curl https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts?circle-token=${CIRCLE_TOKEN} | grep -o 'https://[^"]*')"
+  fi
 
   # Assigning slack messages based on the CircleCI job name
 
