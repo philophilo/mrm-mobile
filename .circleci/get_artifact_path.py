5ca172b3d5f57223a226afd6c7933abdbaa24cc6
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-07-20 13:07:20 +0300

    test

--- .circleci/get_artifact_path.py
+++ .circleci/get_artifact_path.py
@@ -10,11 +10,14 @@ def read_url_file():
 
 def create_path_file():
     url = read_url_file()
+    print url, "+++++++++++++++++++++"
     file_path = re.sub(r"(?<=)(.*)(?=test)", "", url)
+    print file_path, "******************"
     path_file = open("artifact_path", "w+")
     if path_file.mode == "w+":
         path_file.write(file_path)
         path_file.close()
+    print "DONE#####################"
 
 
 if __name__ == "__main__":
